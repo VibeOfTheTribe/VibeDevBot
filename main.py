e31832ee4d4a45022daf68c955337618bfabda1b
PixelKraftDevel <>
2021-06-29 23:08:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import telebot
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+  bot.reply_to(message, "Hey whatup maine")
+
+  bot.polling()
+
